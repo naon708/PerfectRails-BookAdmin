d9b828f37e65937772d57be03deaa60269bac569
naon708 <naoya_nis@icloud.com>
2023-03-19 20:46:35 +0900

    カスタムバリデーションを試した

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,18 @@
 class Book < ApplicationRecord
   scope :costly, -> { where('price > ?', 3000) }
-  scope :written_about, ->(theme) { where('name LIKE ?', "#{theme}") }
+  scope :written_about, ->(theme) { where(':name LIKE ?', "#{theme}") }
 
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 25 }
+  validates :price, numericality: { greater_than_or_eqal_to: 0 }
+
+  # validate do |book|
+  #   if book.name.include?('porn')
+  #     book.errors[:name] << 'Porn is bad.'
+  #   end
+  # end
 end
