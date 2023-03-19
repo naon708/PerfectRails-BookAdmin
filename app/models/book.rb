5e09699043f8bed525a4d70bd10346f086c13df2
naon708 <naoya_nis@icloud.com>
2023-03-19 20:57:45 +0900

    Improve

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,18 +1,21 @@
 class Book < ApplicationRecord
+  ##### Scope #####
   scope :costly, -> { where('price > ?', 3000) }
   scope :written_about, ->(theme) { where(':name LIKE ?', "#{theme}") }
 
+  ##### Relation #####
   belongs_to :publisher
   has_many :book_authors
   has_many :authors, through: :book_authors
 
+  ##### Validation #####
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_eqal_to: 0 }
 
-  # validate do |book|
-  #   if book.name.include?('porn')
-  #     book.errors[:name] << 'Porn is bad.'
-  #   end
-  # end
+  validate do |book|
+    if book.name.include?('porn')
+      book.errors[:name] << 'Porn is bad.'
+    end
+  end
 end
