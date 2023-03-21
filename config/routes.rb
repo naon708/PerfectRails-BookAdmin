4722051a345d7c781d873c4dc1c2582c2955db9f
naon708 <naoya_nis@icloud.com>
2023-03-21 14:56:53 +0900

    Improve Routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'profiles/edit'
-  get 'profiles/update'
   get '/books/:id' => 'books#show'
   delete '/books/:id' => 'books#destroy'
 
   resources :publishers
-  resource :profile
+  resource :profile, only: %i(show edit update)
 
   # resources :publishers do
   #   resources :books
