8b2ef58a63f6bc11a2261e50b4fb5dee6056be69
naon708 <naoya_nis@icloud.com>
2023-03-21 14:20:48 +0900

    Profileを追加(Model/Controller/Routing)

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   get '/books/:id' => 'books#show'
   delete '/books/:id' => 'books#destroy'
 
   resources :publishers
+  resource :profile
 
   # resources :publishers do
   #   resources :books
