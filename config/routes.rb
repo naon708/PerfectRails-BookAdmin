b281e9585d4621264b1f92f3cd635567bf1ceb72
naon708 <naoya_nis@icloud.com>
2023-03-21 14:15:06 +0900

    Routing: resources, member, collection

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
   get '/books/:id' => 'books#show'
   delete '/books/:id' => 'books#destroy'
+
+  resources :publishers
+
+  # resources :publishers do
+  #   resources :books
+
+  #   member do
+  #     get 'detail'
+  #   end
+
+  #   collection do
+  #     get 'search'
+  #   end
+  # end
 end
